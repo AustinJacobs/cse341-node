18ef5f58adafd86b3dc651f8be6dbc198c42f6ab
Austin Jacobs <aust.t.jac@gmail.com>
2022-09-30 09:28:05 -0600

    Initial testing for render

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -33,4 +33,81 @@ const getSingleContact = async (req, res, next) => {
   });
 };
 
-module.exports = { getAllContacts, getSingleContact };
+const createContact = async (req, res) => {
+  const document = {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    favoriteColor: req.body.favoriteColor,
+    birthday: req.body.birthday,
+  };
+  const response = await mongodb
+    .fetchDb()
+    .db(database_name)
+    .collection('contacts')
+    // Can use insertMany to insert multiple documents at once.
+    .insertOne(document);
+  if (response.acknowledged) {
+    res.status(201).json(response);
+  } else {
+    res
+      .status(500)
+      .json(
+        response.error || 'An error ocurred during contact creation.'
+      );
+  }
+};
+
+const updateContact = async (req, res) => {
+  const userId = new ObjectId(req.params.id);
+  // be aware of updateOne if you only want to update specific fields
+  const contact = {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    favoriteColor: req.body.favoriteColor,
+    birthday: req.body.birthday,
+  };
+  const response = await mongodb
+    .fetchDb()
+    .db(database_name)
+    .collection('contacts')
+    .replaceOne({ _id: userId }, contact);
+  console.log(response);
+  if (response.modifiedCount > 0) {
+    res.status(204).send();
+  } else {
+    res
+      .status(500)
+      .json(
+        response.error || 'An error ocurred during the contact update.'
+      );
+  }
+};
+
+const deleteContact = async (req, res) => {
+  const userId = new ObjectId(req.params.id);
+  const response = await mongodb
+    .fetchDb()
+    .db(database_name)
+    .collection('contacts')
+    .deleteOne({ _id: userId }, true);
+  console.log(response);
+  if (response.deletedCount > 0) {
+    res.status(200).send();
+  } else {
+    res
+      .status(500)
+      .json(
+        response.error || 'An error ocurred during contact deletion.'
+      );
+  }
+};
+
+module.exports = {
+  getAllContacts,
+  getSingleContact,
+  createContact,
+  updateContact,
+  deleteContact,
+};
