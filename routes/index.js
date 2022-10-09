45e7654b7d0635bb5c394cc98b85a2471a36353b
Austin Jacobs <aust.t.jac@gmail.com>
2022-10-08 22:43:42 -0600

    Added CORS through middleware.

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,18 @@
 const express = require('express');
 const router = express.Router();
-const cors = require('cors');
+const openCors = require('../middleware/openCors');
 
-router.use(cors);
+router.use(openCors);
 router.use('/api-docs', require('./swagger'));
 router.use('/contacts', require('./contacts'));
-// router.use(
-//   '/',
-//   (docData = (req, res) => {
-//     let docData = {
-//       documentationURL: 'https://cse341-node-br28.onrender.com-api-docs',
-//     };
-//     res.send(docData);
-//   })
-// );
+router.use(
+  '/',
+  (docData = (req, res) => {
+    let docData = {
+      documentationURL: 'https://cse341-node-br28.onrender.com-api-docs',
+    };
+    res.send(docData);
+  })
+);
 
 module.exports = router;
