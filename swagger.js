99a002eed02d80b6a717e35968a830851c212438
Austin Jacobs <aust.t.jac@gmail.com>
2022-10-08 23:09:03 -0600

    Render test with updated paths.

--- swagger.js
+++ swagger.js
@@ -5,12 +5,12 @@ const doc = {
     title: 'Contacts API',
     description: 'Contacts API',
   },
-  host: 'https://cse341-node-br28.onrender.com', // add render host url
-  schemes: ['https'], // won't run on http. Render runs on https
+  host: '', // add render host url
+  schemes: ['https', 'http'], // won't run on http. Render runs on https
 };
 
-const outputFile = './swagger.json';
-const endpointsFiles = ['./routes/index.js'];
+const outputFile = 'swagger.json'; // ./swagger.json
+const endpointsFiles = ['./index.js']; //./routes/index.js
 
 // generate swagger.json
 swaggerAutogen(outputFile, endpointsFiles, doc);
